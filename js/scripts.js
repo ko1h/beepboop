3c9be22262d1ee289d94cc4b6dce641869e9f054
kohichi horii <kohichi.horii@gmail.com>
2018-10-26 16:33:00 -0700

    changing foreach to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,42 @@
 function robotNumber(number) {
-  console.log("this is working")
+  var newSomthing = [];
+  for (var i = 0; i < something; i++) {
+    newSomething.push(i);
+  }
+  return number;
+}
 
-  var array = [...Array(number + 1).keys()];
-  console.log(array)
+  // console.log("this is working")
+  //
+  // var array = [...Array(number + 1).keys()];
+  // console.log(array)
+  //
+  // var newArray = array.toString();
+  // console.log(newArray)
+  //
+  // var splitArray = newArray.split("")
+  // console.log(splitArray)
 
-  var newArray = array.toString();
-  console.log(newArray)
 
-  var splitArray = newArray.split("")
-  console.log(splitArray)
 
-  var numberOfInputs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var robotSound = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"]
+  // var numberOfInputs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  // var robotSound = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"]
+  //
+  // number = [];
+  //
+  // console.log (robotSound);
+  // for (var i = 0; i < splitArray.length; i++) {
+  //     if (parseInt(robotSound[0]) === i) {
+  //       var Result4 = robotSound[i];
+  //     console.log (Result4)
+  //     }
+  //   }
 
-  console.log (robotSound);
-
-  numberOfInputs.forEach(function(numberOfInputs) {
-    // robotSound.forEach(function(robotSound){
-    //   console.log(robotSound)
-    // });
-  });
+  // numberOfInputs.forEach(function(numberOfInputs) {
+  //   robotSound.forEach(function(robotSound){
+  //     console.log(robotSound)
+  //   });
+  // });
 }
 
 $(document).ready(function() {
