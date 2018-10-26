bf255064a60cc59602adf46db0957e0b6e655596
kohichi horii <kohichi.horii@gmail.com>
2018-10-26 10:41:42 -0700

    Add Array().keys() method

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
+function robotNumber(number) {
+  console.log("this is working")
+  var arrayNumber = [...Array(number).keys()];
+  console.log(arrayNumber)
+  // var array = new Array();
+  //   for(var i = start; i < end; i++)
+  //   {
+  //     array.push(i);
+  //   }
+  //   return array;
+}
+
+
+
 $(document).ready(function() {
   $("form#convertor").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#inputNumber").val());
-    var result = counter(number);
+    var result = robotNumber(number);
 
     $("#result").show();
     $("#result").text(result);
-    console.log(result)
+    console.log(number)
   });
 });
