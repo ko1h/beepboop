cae7681b678f28b98a7a2d2fea15e82cec84f356
kohichi horii <kohichi.horii@gmail.com>
2018-10-26 15:52:49 -0700

    fixing foreach problems

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function robotNumber(number) {
   console.log("this is working")
+
   var array = [...Array(number + 1).keys()];
   console.log(array)
 
@@ -9,23 +10,17 @@ function robotNumber(number) {
   var splitArray = newArray.split("")
   console.log(splitArray)
 
-  var robotSound = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"];
-  var number= [];
+  var numberOfInputs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var robotSound = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"]
 
-  robotSound.forEach(function (robotSound) {
-    console.log(robotSound)
-  }
+  console.log (robotSound);
 
-  // var number []
-  //
-  // {
-  //   return array
-  // }
-
+  numberOfInputs.forEach(function(numberOfInputs) {
+    // robotSound.forEach(function(robotSound){
+    //   console.log(robotSound)
+    // });
+  });
 }
-
-
-
 
 $(document).ready(function() {
   $("form#convertor").submit(function(event) {
