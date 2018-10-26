f900283475fb348dd8c08a8bf58d112d5e985c71
kohichi horii <kohichi.horii@gmail.com>
2018-10-26 14:57:22 -0700

    fixing error when using loops in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 function robotNumber(number) {
   console.log("this is working")
-
   var array = [...Array(number + 1).keys()];
   console.log(array)
 
   var newArray = array.toString();
   console.log(newArray)
 
-  if (number <= 0 && number >= 10) {
-    var splitArray = newArray.split("");
-    console.log(splitArray)
-    for (var i = 0; i < arrayFirstTen.length; i++) {
-      if (parseInt(splitArray[]) === i) {
-        var arrayResult4 = arrayFirstTen[i];
-      }
-  return arrayResult4;
+  var splitArray = newArray.split("")
+  console.log(splitArray)
+
+  // for (var i = 0; i < splitArray.length; i++) {
+  //   if (parseInt(splitArray[]) === i) {
+  //     var arrayResult = arrayFirstTen[i];
+  //   }
+  {
+    return array
+  }
+  // var arrayFirstTen = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"];
 }
-var arrayFirstTen = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"];
+
+
 
 
 $(document).ready(function() {
