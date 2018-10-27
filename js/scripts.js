dba60aa3b5e774e99431630210a20e994c72856b
kohichi horii <kohichi.horii@gmail.com>
2018-10-27 13:51:09 -0700

    fixing forloop

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,51 @@
+
 function robotNumber(number) {
-  var newSomthing = [];
-  for (var i = 0; i < something; i++) {
-    newSomething.push(i);
+  console.log("this is working")
+
+  var array = [...Array(number + 1).keys()];
+  console.log(array)
+
+  var newArray = array.toString();
+  console.log(newArray)
+
+
+  var splitArray = newArray.split("");
+  console.log(splitArray)
+
+  var robotSound = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"]
+
+  var resultArray = []
+
+  for (var i = 0; i <= splitArray.length + 1; i ++) {
+    if (splitArray[i] === ("1")[i]) {
+      resultArray.push(boop[i]);
+    }
+    else if (splitArray[i] === ("0")[i]) {
+      resultArray.push(beep[i]);
+    }
+    console.log(resultArray)
   }
-  return number;
+  return ;
 }
 
-  // console.log("this is working")
-  //
-  // var array = [...Array(number + 1).keys()];
-  // console.log(array)
-  //
-  // var newArray = array.toString();
-  // console.log(newArray)
-  //
-  // var splitArray = newArray.split("")
-  // console.log(splitArray)
-
-
-
-  // var numberOfInputs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  // var robotSound = ["beep", "boop", "2", "I'm sorry, Dave. I'm afraid I can't do that", "4", "5", "6", "7", "8", "9"]
-  //
-  // number = [];
-  //
-  // console.log (robotSound);
-  // for (var i = 0; i < splitArray.length; i++) {
-  //     if (parseInt(robotSound[0]) === i) {
-  //       var Result4 = robotSound[i];
-  //     console.log (Result4)
-  //     }
-  //   }
-
-  // numberOfInputs.forEach(function(numberOfInputs) {
-  //   robotSound.forEach(function(robotSound){
-  //     console.log(robotSound)
-  //   });
-  // });
-}
+//   var numberOfInputs = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+//
+//   number = [];
+//
+//   console.log (robotSound);
+//   for (var i = 0; i < splitArray.length; i++) {
+//       if (parseInt(robotSound[0]) === i) {
+//         var Result4 = robotSound[i];
+//       console.log (Result4)
+//       }
+//     }
+//
+//   numberOfInputs.forEach(function(numberOfInputs) {
+//     robotSound.forEach(function(robotSound){
+//       console.log(robotSound)
+//     });
+//   });
+// }
 
 $(document).ready(function() {
   $("form#convertor").submit(function(event) {
