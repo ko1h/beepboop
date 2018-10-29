278f98bc683864e4d6f62abcbc6fa76b51e53086
kohichi horii <kohichi.horii@gmail.com>
2018-10-28 19:58:09 -0700

    code is able to change one number at a time

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,36 @@
-function robotNumber(inputNum) {
-    console.log("this is working")
+function robotSounds(inputNum) {
+  console.log("this is working")
 
-    var newArray = inputNum.toString();
-    console.log(newArray)
+  var newArray = inputNum.toString();
+  console.log(newArray)
 
-    if (inputNum === 3) {
-      return "I'm sorry, Dave. I'm afraid I can't do that.";
-    }
-    else if (newArray.includes("0")) {
-      return "beep";
-    }
-    else if (newArray.includes("1")) {
-      return "boop";
-    }
-    return inputNum
-    console.log(newArray)
+  var splitArray = newArray.split("")
+  console.log(splitArray)
+
+    var arrayResult = [];
+
+    for (var i = 0; i < splitArray.length; i++) {
+      console.log(splitArray)
+      if (splitArray.includes("3")) {
+        arrayResult.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      }
+      else if (splitArray.includes("0")) {
+        arrayResult.push("beep");
+      }
+      else if (splitArray.includes("1")) {
+        arrayResult.push("boop");
+      }
+      return arrayResult
+  }
 }
-
-// function rebotSounds(inputNum) {
-//   console.log("second function")
-//
-//   var arrayResult = [];
-//
-//     for (var i = 0; i < splitArray.length; i++) {
-//   }
-// }
 
 
 $(document).ready(function() {
   $("form#convertor").submit(function(event) {
     event.preventDefault();
-    var inputNum = parseInt($("input#inputNumber").val());
-    var result = robotNumber(inputNum);
+    var inputNum = $("input#inputNumber").val();
+    var result = robotSounds(inputNum);
+
     $("#result").show();
     $("#result").text(result);
     console.log(result)
