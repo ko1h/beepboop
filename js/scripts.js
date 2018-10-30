908be828e14ed67a7444a534169bf6369c300e74
kohichi horii <kohichi.horii@gmail.com>
2018-10-30 16:24:45 -0700

    changed input in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,26 @@
 function robotSounds(inputNum) {
   console.log("this is working")
 
-  var newArray = inputNum.toString();
-  console.log(newArray)
-
-  var splitArray = newArray.split("")
-  console.log(splitArray
-
   var arrayResult = [];
 
-    // for (var i = 0; i < splitArray.length; i++) {
-      console.log(i)
-      if (splitArray.includes("3")) {
+    for (var i = 0; i < inputNum; i++) {
+
+      if (i % 3 == 0 && i !== 0) {
         arrayResult.push("I'm sorry, Dave. I'm afraid I can't do that.");
       }
-      else if (splitArray.includes("0")) {
-        arrayResult.push("beep");
-      }
-      else if (splitArray.includes("1")) {
+      else if (i.toString().split("").includes("1")) {
         arrayResult.push("boop");
         console.log(arrayResult)
       }
-      // else {
-      //   return n
-      // }
-      return arrayResult
+      else if (i.toString().split("").includes("0")) {
+        arrayResult.push("beep");
+      }
+      else {
+        arrayResult.push(" " + i.toString())
+      }
   }
-}
+  return arrayResult
+};
 
 
 $(document).ready(function() {
